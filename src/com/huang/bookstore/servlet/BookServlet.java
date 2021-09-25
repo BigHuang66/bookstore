d7f375f975cd9652536deaaec1e51043f20522b8
BigHuang66 <jc_huang528@163.com>
2021-09-25 16:40:09 +0800

    修改servlet

--- src/com/huang/bookstore/servlet/BookServlet.java
+++ src/com/huang/bookstore/servlet/BookServlet.java
@@ -1,5 +1,9 @@
 package com.huang.bookstore.servlet;
 
+import com.huang.bookstore.service.AccountService;
+import com.huang.bookstore.service.BookService;
+import com.huang.bookstore.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +17,15 @@ import java.io.IOException;
  * @date 2021/9/25 15:39
  */
 public class BookServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private BookService bookService = new BookService();
+
+    private UserService userService = new UserService();
+
+    private AccountService accountService = new AccountService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
